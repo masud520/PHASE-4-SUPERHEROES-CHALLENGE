62fa66bc054fc0cfa0465a9523e979c80196aba5
masud520 <masud.abdi@student.moringaschool.com>
2024-02-01 12:34:51 +0300

    updated  models

--- python-code-challenge-superheroes/code-challenge/app/models.py
+++ python-code-challenge-superheroes/code-challenge/app/models.py
@@ -10,7 +10,7 @@ class Hero(db.Model):
     name = db.Column(db.String(255), nullable=False)
     super_name = db.Column(db.String(255), nullable=False)
 
-    heropowers = db.relationship('Power', secondary='hero_powers', backref='heroes')
+    heropowers = db.relationship('HeroPower', backref='hero', lazy=True)
 
     def __repr__(self):
         return f'<Hero {self.name}>'
@@ -22,19 +22,11 @@ class Power(db.Model):
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
 
-    heropowers = db.relationship('Power', secondary='hero_powers', backref='powers')
-
+    heropowers = db.relationship('HeroPower', backref='power', lazy=True)
 
     @validates('description')
     def validate_description(self, key, value):
-        # if value is a string
-        if not isinstance(value, str):
-            raise ValueError("Description must be a string.")
-
-        # if the length of the string is between 5 and 255 characters
-        if not (5 <= len(value) <= 255):
-            raise ValueError("Description must be a string with a length between 5 and 255 characters.")
-
+        # your validation logic
         return value
 
     def __repr__(self):
@@ -46,20 +38,12 @@ class HeroPower(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
-    strength = db.Column(db.Integer, nullable=False)
+    strength = db.Column(db.String, nullable=False)
 
     @validates('strength')
     def validate_strength(self, key, value):
-        # Check if strength is an integer
-        if not isinstance(value, int):
-            raise ValueError("Strength must be an integer.")
-
-        # Check if strength is a positive integer
-        if value < 0:
-            raise ValueError("Strength must be a positive integer.")
-
+        # your validation logic
         return value
 
     def __repr__(self):
         return f'<HeroPower hero_id={self.hero_id}, power_id={self.power_id}, strength={self.strength}>'
-
